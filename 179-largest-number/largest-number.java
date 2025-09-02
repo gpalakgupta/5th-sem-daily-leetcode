1376a355ce613cb452d579acd0b9c95a6873cda0
Palak gupta <151661890+gpalakgupta@users.noreply.github.com>
2025-09-02 14:07:34 +0530

    Time: 14 ms (5.84%) | Memory: 45.3 MB (6.04%) - LeetSync

--- 179-largest-number/largest-number.java
+++ 179-largest-number/largest-number.java
@@ -1,42 +1,42 @@
 class Solution {
-    public boolean isvalid(String a, String b){
-        String c = a+b;
-        String d = b+a;
-        return c.compareTo(d)<0;
+    public static boolean isValid( String  i , String j){
+         String c =  i+j;
+         String d = j+i;
+         return c.compareTo(d)>0;
     }
-    public void sort(String[] str){
-        for(int i = 1; i<str.length; i++){
-            for(int j = 0; j<str.length-i; j++){
-                if(isvalid(str[j],str[j+1])){
-                    String t = str[j];
-                    str[j] = str[j+1];
-                    str[j+1] = t;
-                }
+    public static void sort(String[] str){
+        for(int i = 0 ; i < str.length-1 ; i++){
+            for(int j = 0 ; j < str.length-i-1 ; j++)
+            if(!isValid(str[j] , str[j+1])){
+                String temp = str[j];
+                str[j] = str[j+1];
+                str[j+1] = temp;
             }
         }
     }
     public String largestNumber(int[] nums) {
-        int n = nums.length;
-        String[] str = new String[n];
-        for(int i = 0; i<n; i++){
+        String[] str = new String[nums.length];
+        for(int i = 0 ; i < nums.length ; i++){
             str[i] = nums[i]+"";
+
         }
-        sort(str); //[2,10]
-        int c = 0;
-        String ans = "";
-        for(String el : str){
-            ans+=el;
-            if(el.equals("0")){
-                c++;
-            }
+        sort(str);
+
+    String ans = "";
+    int c = 0;
+    for(int i = 0 ; i  < str.length ; i++){
+        ans+=str[i];
+        if(str[i].equals( "0")){
+            c++;
         }
-        // [0,0] 2
-        // ans = 00
-        // int 01 = 1
-        if(c == n){
-            long t = Long.parseLong(ans);
-            return t+"";
-        }
-        return ans;
+
+
+        
+    }
+    if(c == nums.length){
+         long t = Long.parseLong(ans);
+            return t + "";
+    }
+return ans ;
     }
 }
