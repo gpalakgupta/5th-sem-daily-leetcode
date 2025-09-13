cf251a0af32069f995725fd761aa41b434184ab2
Palak gupta <151661890+gpalakgupta@users.noreply.github.com>
2025-09-13 10:05:56 +0530

    Time: 21 ms (27.90%) | Memory: 45.4 MB (29.37%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.java
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.java
@@ -1,25 +1,43 @@
 class Solution {
     public int numTilePossibilities(String tiles) {
         int n = tiles.length();
-        int[] freq = new int[26];
-        for(int i = 0; i<n; i++){
-            char ch = tiles.charAt(i);
-            freq[ch-'A']++;
-        }
+        // int[] freq = new int[26];
+        // for(int i = 0; i<n; i++){
+        //     char ch = tiles.charAt(i);
+        //     freq[ch-'A']++;
+        // }
 
-        helper(freq);
-        return count;
+        // helper(freq);
+        // return count;
+        boolean[] visited = new boolean[n];
+        StringBuilder sb = new StringBuilder();
+        HashSet<String> st = new HashSet<>();
+        helper(tiles,sb,st,visited);
+        return st.size()-1;
     }
-    int count = -1;
-    public void helper(int[] freq){
-        count++;
-        for(int i = 0; i<26; i++){
-            if(freq[i] == 0){
+    public void helper(String tiles,  StringBuilder sb, HashSet<String> st, boolean[] visited){
+        st.add(sb.toString());
+        for(int i = 0; i<tiles.length(); i++){
+            if(visited[i] == true){
                 continue;
             }
-            freq[i]--;
-            helper(freq);
-            freq[i]++;
+            visited[i] = true;
+            sb.append(tiles.charAt(i));
+            helper(tiles,sb,st,visited);
+            visited[i] = false;
+            sb.deleteCharAt(sb.length()-1);
         }
     }
+    // int count = -1;
+    // public void helper(int[] freq){
+    //     count++;
+    //     for(int i = 0; i<26; i++){
+    //         if(freq[i] == 0){
+    //             continue;
+    //         }
+    //         freq[i]--;
+    //         helper(freq);
+    //         freq[i]++;
+    //     }
+    // }
 }
