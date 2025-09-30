5f14af72154b8c6e003b3b496b854bcaf47c5688
Palak gupta <151661890+gpalakgupta@users.noreply.github.com>
2025-09-30 16:39:34 +0530

    Time: 3 ms (11.15%) | Memory: 49.3 MB (5.26%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,29 +1,30 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][] dp = new int[n+1][2];
+        int[][] dp = new int[n][2];
         for(int[] a : dp){
             Arrays.fill(a,-1);
         }
-        return helper(0,1,n,prices,dp);
+        return helper(prices,0,0,dp);
     }
-    public int helper(int idx, int buy, int n,int[] prices, int[][] dp){
-        if(idx == n){
+    public int helper(int[] prices, int idx, int buy, int[][] dp){
+        if(idx >= prices.length){
             return 0;
         }
         if(dp[idx][buy] != -1){
             return dp[idx][buy];
         }
+
         int profit = 0;
-        if(buy == 1){
-            int take = -prices[idx]+helper(idx+1,0,n,prices,dp);
-            int notake = helper(idx+1,1,n,prices,dp);
-            profit = Math.max(take,notake);
+        if(buy == 0){
+            int take = -prices[idx]+helper(prices,idx+1,1,dp);
+            int notake = helper(prices,idx+1,buy,dp);
+            profit  = Math.max(take,notake);
         }
         else{
-            int sell = prices[idx]+helper(idx+1,1,n,prices,dp);
-            int nosell = helper(idx+1,0,n,prices,dp);
-            profit = Math.max(sell,nosell);
+            int sell = prices[idx]+helper(prices,idx+1,0,dp);
+            int nosell = helper(prices,idx+1,buy,dp);
+            profit  = Math.max(sell,nosell);
         }
         return dp[idx][buy] = profit;
     }
