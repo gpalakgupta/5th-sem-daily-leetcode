52b89693413e7152fdda096fbc20c62564c581d0
Palak gupta <151661890+gpalakgupta@users.noreply.github.com>
2025-09-30 17:05:41 +0530

    Time: 3 ms (11.15%) | Memory: 45.9 MB (48.43%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,31 +1,53 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][] dp = new int[n][2];
-        for(int[] a : dp){
-            Arrays.fill(a,-1);
-        }
-        return helper(prices,0,0,dp);
-    }
-    public int helper(int[] prices, int idx, int buy, int[][] dp){
-        if(idx >= prices.length){
-            return 0;
-        }
-        if(dp[idx][buy] != -1){
-            return dp[idx][buy];
-        }
+    //     int[][] dp = new int[n][2];
+    //     for(int[] a : dp){
+    //         Arrays.fill(a,-1);
+    //     }
+    //     return helper(prices,0,0,dp);
 
-        int profit = 0;
-        if(buy == 0){
-            int take = -prices[idx]+helper(prices,idx+1,1,dp);
-            int notake = helper(prices,idx+1,buy,dp);
-            profit  = Math.max(take,notake);
-        }
-        else{
-            int sell = prices[idx]+helper(prices,idx+1,0,dp);
-            int nosell = helper(prices,idx+1,buy,dp);
-            profit  = Math.max(sell,nosell);
-        }
-        return dp[idx][buy] = profit;
+        return helper(prices);
     }
+
+    public int helper(int[] prices){
+        int n = prices.length;
+        int[][] dp = new int[n+1][2];
+        int ans = 0;
+        for(int i = n-1;  i>=0; i--){
+            for(int j = 0; j<2; j++){
+                if(j == 0){
+                    ans = Math.max(-prices[i]+dp[i+1][1],dp[i+1][0]);
+                }
+                else{
+                    ans = Math.max(prices[i]+dp[i+1][0],dp[i+1][1]);
+                }
+            dp[i][j] = ans;
+
+            }
+        }
+        return dp[0][0];
+    }
+
+    // public int helper(int[] prices, int idx, int buy, int[][] dp){
+    //     if(idx >= prices.length){
+    //         return 0;
+    //     }
+    //     if(dp[idx][buy] != -1){
+    //         return dp[idx][buy];
+    //     }
+
+    //     int profit = 0;
+    //     if(buy == 0){
+    //         int take = -prices[idx]+helper(prices,idx+1,1,dp);
+    //         int notake = helper(prices,idx+1,buy,dp);
+    //         profit  = Math.max(take,notake);
+    //     }
+    //     else{
+    //         int sell = prices[idx]+helper(prices,idx+1,0,dp);
+    //         int nosell = helper(prices,idx+1,buy,dp);
+    //         profit  = Math.max(sell,nosell);
+    //     }
+    //     return dp[idx][buy] = profit;
+    // }
 }
