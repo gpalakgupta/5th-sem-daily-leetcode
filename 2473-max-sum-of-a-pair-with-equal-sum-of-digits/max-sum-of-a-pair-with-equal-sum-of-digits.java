eeceab7d893438baee22ec137ac7fee0a8ac398c
Palak gupta <151661890+gpalakgupta@users.noreply.github.com>
2025-10-01 15:27:45 +0530

    Time: 104 ms (10.94%) | Memory: 63.6 MB (6.14%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -1,12 +1,4 @@
 class Solution {
-    public class Pair{
-        int el;
-        int s;
-        public Pair(int el, int s){
-            this.el = el;
-            this.s = s;
-        }
-    }
     public int sum(int el){
         int ans = 0;
         while(el != 0){
@@ -16,13 +8,6 @@ class Solution {
         return ans;
     }
     public int maximumSum(int[] nums) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>(new Comparator<Pair>(){
-            @Override
-            public int compare(Pair o1, Pair o2){
-                return o2.el-o1.el;
-            }
-        });
-        
         HashMap<Integer,List<Integer>> map = new HashMap<>();
         for(int el : nums){
             int sod = sum(el);
