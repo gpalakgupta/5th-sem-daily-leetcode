1d1834c0a2651521277139c893d62e056ec303d3
Palak gupta <151661890+gpalakgupta@users.noreply.github.com>
2025-10-01 14:12:18 +0530

    Time: 3 ms (80.30%) | Memory: 45.4 MB (91.46%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -4,6 +4,7 @@ class Solution {
         int[] pre = new int[n];
         int[] suff = new int[n];
         pre[0] = 1;
+        suff[n-1] = 1;
         for(int i = 1; i<n; i++){
             if(ratings[i] > ratings[i-1]){
                 pre[i] = pre[i-1]+1;
@@ -12,7 +13,6 @@ class Solution {
                 pre[i] = 1;
             }
         }
-        suff[n-1] = 1;
         for(int i = n-2; i>=0; i--){
             if(ratings[i] > ratings[i+1]){
                 suff[i] = suff[i+1]+1;
@@ -21,10 +21,10 @@ class Solution {
                 suff[i] = 1;
             }
         }
-        int sum  = 0;
+        int ans = 0;
         for(int i = 0; i<n; i++){
-            sum+=Math.max(pre[i],suff[i]);
+            ans += Math.max(pre[i],suff[i]);
         }
-        return sum;
+        return ans;
     }
 }
