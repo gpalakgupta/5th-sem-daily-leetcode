828658e98934df79d62e6aa8858ddc0ba7a06937
Palak gupta <151661890+gpalakgupta@users.noreply.github.com>
2025-10-04 09:08:40 +0530

    Time: 50 ms (31.79%) | Memory: 64.5 MB (26.49%) - LeetSync

--- 1517-restore-the-array/restore-the-array.java
+++ 1517-restore-the-array/restore-the-array.java
@@ -1,33 +1,34 @@
 class Solution {
-    int n;
     int mod = 1000000007;
+    int n;
     public int numberOfArrays(String s, int k) {
         n = s.length();
         long[] dp = new long[n+1];
-        Arrays.fill(dp,-1);
-        return (int)helper(0,s,k,dp);
+         
+            Arrays.fill(dp,-1);
+         
+        return (int)helper(s,k,0,dp);
     }
-    public long helper(int idx, String str, int k, long[] dp){
+    public long helper(String s, int k, int idx, long[] dp){
         if(idx >= n){
             return 1;
         }
-        if(str.charAt(idx) == '0'){
+        if(s.charAt(idx) == '0'){
             return 0;
         }
-
         if(dp[idx] != -1){
             return dp[idx];
-        }
+        } 
 
-        long ans = 0;
         long num = 0;
+        long ans = 0;
         for(int i = idx; i<n; i++){
-            num = num*10+(str.charAt(i)-'0');
+            num = num*10+(s.charAt(i)-'0');
             if(num > k){
                 break;
             }
-            ans = (ans % mod + helper(i+1,str,k,dp)%mod)%mod;
+            ans = (ans%mod+helper(s,k,i+1,dp)%mod)%mod;
         }
-        return dp[idx] = ans;
+        return dp[idx] = ans%mod;
     }
 }
