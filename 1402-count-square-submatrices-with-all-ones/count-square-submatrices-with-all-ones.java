ddde89459e47fbb8cb298ef304b66eae70b2e81a
Palak gupta <151661890+gpalakgupta@users.noreply.github.com>
2025-10-06 11:31:00 +0530

    Time: 5 ms (97.08%) | Memory: 55.3 MB (52.78%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -5,7 +5,7 @@ class Solution {
         int[][] dp = new int[n][m];
         for(int i = 0; i<n; i++){
             dp[i][0] = matrix[i][0];
-        }   
+        }
         for(int j = 0; j<m; j++){
             dp[0][j] = matrix[0][j];
         }
@@ -15,14 +15,14 @@ class Solution {
                     dp[i][j] = 0;
                 }
                 else{
-                    dp[i][j] = Math.min(dp[i-1][j],Math.min(dp[i][j-1],dp[i-1][j-1]))+1;
+                    dp[i][j] = Math.min(dp[i][j-1],Math.min(dp[i-1][j],dp[i-1][j-1]))+1;
                 }
             }
         }
         int sum = 0;
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
-                sum+=dp[i][j];
+                sum += dp[i][j];
             }
         }
         return sum;
