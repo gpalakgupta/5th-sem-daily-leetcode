773c447f3e309eb3502e1ffda81e82e2321c197a
Palak gupta <151661890+gpalakgupta@users.noreply.github.com>
2025-10-06 10:44:56 +0530

    Time: 13 ms (40.36%) | Memory: 44.4 MB (60.84%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.java
+++ 794-swim-in-rising-water/swim-in-rising-water.java
@@ -13,30 +13,31 @@ class Solution {
         PriorityQueue<Pair> pq = new PriorityQueue<>(new Comparator<Pair>(){
             @Override
             public int compare(Pair o1, Pair o2){
-                return o1.val-o2.val;
+                return o1.val - o2.val;
             }
         });
+
+        int ans = -1;
+        pq.add(new Pair(grid[0][0],0,0));
         int n = grid.length;
         boolean[][] visited = new boolean[n][n];
-        pq.add(new Pair(grid[0][0],0,0));
-        int[] row = {0,-1,0,1};
-        int[] col = {-1,0,1,0};
-        int ans = 0;
+        int[] r = {0,-1,0,1};
+        int[] c = {-1,0,1,0};
         while(!pq.isEmpty()){
             Pair rv = pq.poll();
-            int r = rv.i;
-            int c = rv.j;
             int val = rv.val;
+            int row = rv.i;
+            int col = rv.j;
             ans = Math.max(ans,val);
-            if(r == n-1 && c == n-1){
+            if(row == n-1 && col == n-1){
                 return ans;
             }
+            visited[row][col] = true;
             for(int i = 0; i<4; i++){
-                int cr = r+row[i];
-                int cc = c+col[i];
-                if(cr >= 0 && cr < n && cc >= 0 && cc < n && !visited[cr][cc]){
+                int cr = row+r[i];
+                int cc = col+c[i];
+                if(cr >= 0 && cc >= 0 && cr < n && cc < n && (visited[cr][cc] == false)){
                     pq.add(new Pair(grid[cr][cc],cr,cc));
-                    visited[cr][cc] = true;
                 }
             }
         }
