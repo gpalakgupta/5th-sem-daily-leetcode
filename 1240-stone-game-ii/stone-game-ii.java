37ca1eb93cf65d4b28f0a51c70993ec61955eb96
Palak gupta <151661890+gpalakgupta@users.noreply.github.com>
2025-10-07 13:24:27 +0530

    Time: 12 ms (29.50%) | Memory: 45 MB (15.75%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.java
+++ 1240-stone-game-ii/stone-game-ii.java
@@ -8,29 +8,27 @@ class Solution {
                 Arrays.fill(b,-1);
             }
         }
-
         return helper(piles,0,0,1,dp);
     }
-    public int helper(int[] piles, int idx, int person, int m, int[][][] dp){
+    public int helper(int[] piles, int person, int idx, int m, int[][][] dp){
         if(idx >= n){
             return 0;
         }
-
         if(dp[person][idx][m] != -1){
             return dp[person][idx][m];
         }
 
-        int res = (person == 0) ? -1 : Integer.MAX_VALUE;
+        int res = (person == 0) ?  -1 : Integer.MAX_VALUE;
         int stone = 0;
-        for(int i = 1; i<=Math.min(2*m,n-idx); i++){
+        for(int i = 1; i<= Math.min(2*m,n-idx); i++){
             stone += piles[idx+i-1];
             if(person == 0){
-                res = Math.max(res,stone+helper(piles,idx+i,1,Math.max(m,i),dp));
+                res = Math.max(res,stone+helper(piles,1,idx+i,Math.max(m,i),dp));
             }
             else{
-                res = Math.min(res,helper(piles,idx+i,0,Math.max(m,i),dp));
+                res = Math.min(res,helper(piles,0,idx+i,Math.max(m,i),dp));
             }
         }
-        return dp[person][idx][m]  = res;
+        return dp[person][idx][m] = res;
     }
 }
