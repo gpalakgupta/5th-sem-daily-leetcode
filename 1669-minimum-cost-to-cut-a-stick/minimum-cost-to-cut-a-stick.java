566c0edf0310a79c348724ad959a0ed201e37196
Palak gupta <151661890+gpalakgupta@users.noreply.github.com>
2025-10-09 23:16:43 +0530

    Time: 23 ms (47.61%) | Memory: 42.9 MB (28.89%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -1,31 +1,30 @@
 class Solution {
     public int minCost(int n, int[] cuts) {
-        int c = cuts.length;
-        int[] arr = new int[c+2];
-        arr[0] = 0;
         Arrays.sort(cuts);
-        for(int i = 0; i<c; i++){
+        int[] arr = new int[cuts.length+2];
+        arr[0] = 0;
+        for(int i = 0; i<cuts.length; i++){
             arr[i+1] = cuts[i];
         }
-        arr[c+1] = n;
+        arr[arr.length-1] = n;
         int[][] dp = new int[arr.length+1][arr.length+1];
         for(int[] a : dp){
             Arrays.fill(a,-1);
-        }
-        return helper(1,c,arr,dp);
+        } 
+        return helper(1,cuts.length,arr,dp);
     }
     public int helper(int i, int j, int[] arr, int[][] dp){
-        if(i>j){
+        if(i > j){
             return 0;
         }
         if(dp[i][j] != -1){
             return dp[i][j];
         }
-        int mini = Integer.MAX_VALUE;
+        int min = Integer.MAX_VALUE;
         for(int k = i; k<=j; k++){
-            int cost = arr[j+1]-arr[i-1]+helper(i,k-1,arr,dp)+helper(k+1,j,arr,dp);
-            mini = Math.min(mini,cost);
+            int res = arr[j+1]-arr[i-1]+helper(i,k-1,arr,dp)+helper(k+1,j,arr,dp);
+            min = Math.min(res,min);
         }
-        return dp[i][j] = mini;
+        return dp[i][j] = min;
     }
 }
