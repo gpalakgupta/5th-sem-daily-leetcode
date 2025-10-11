c06fe70f04b256d39ecf59f94dd2d3c1220011f8
Palak gupta <151661890+gpalakgupta@users.noreply.github.com>
2025-10-12 00:43:06 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (95.01%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -5,17 +5,17 @@ class Solution {
         for(int[] a : dp){
             Arrays.fill(a,-1);
         }
-        return helper(nums,dp,0,n-1) >= 0;
+        return helper(nums,0,n-1,dp) >= 0;
     }
-    public int helper(int[] nums, int[][] dp, int i, int j){
-        if(i>j){
+    public int helper(int[] nums, int i, int j, int[][] dp){
+        if(i > j){
             return 0;
         }
         if(dp[i][j] != -1){
             return dp[i][j];
         }
-        int a = nums[i] - helper(nums,dp,i+1,j);
-        int b = nums[j] - helper(nums,dp,i,j-1);
+        int a = nums[i] - helper(nums,i+1,j,dp);
+        int b = nums[j] - helper(nums,i,j-1,dp);
         return dp[i][j] = Math.max(a,b);
     }
 }
