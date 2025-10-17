5a7fdbc959e01e7af3eda1e0197c9c1a3254d3bc
Palak gupta <151661890+gpalakgupta@users.noreply.github.com>
2025-10-17 09:36:59 +0530

    Time: 4 ms (95.59%) | Memory: 44.8 MB (40.42%) - LeetSync

--- 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.java
+++ 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.java
@@ -1,16 +1,16 @@
 class Solution {
     public int minTaps(int n, int[] ranges) {
-        int N = ranges.length;
         int[] arr = new int[n+1];
-        for(int i = 0; i<N; i++){
-           int st = Math.max(0,i-ranges[i]);
-           int end = Math.min(n,i+ranges[i]);
-           arr[st] = Math.max(arr[st],end);
+        for(int i = 0; i<=n; i++){
+            int st = Math.max(i-ranges[i],0);
+            int end = Math.min(i+ranges[i],n);
+            arr[st] = Math.max(arr[st],end);
         }
+
         int curr = 0;
-        int taps = 0;
         int mx = 0;
-        for(int i = 0; i<N; i++){
+        int taps = 0;
+        for(int i = 0; i<=n; i++){
             if(i > mx){
                 return -1;
             }
