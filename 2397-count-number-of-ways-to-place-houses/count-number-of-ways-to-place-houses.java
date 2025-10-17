a4b43c76891bc40f278670e6c2f85d49ff2e616d
Palak gupta <151661890+gpalakgupta@users.noreply.github.com>
2025-10-17 11:31:05 +0530

    Time: 12 ms (14.13%) | Memory: 44.1 MB (33.15%) - LeetSync

--- 2397-count-number-of-ways-to-place-houses/count-number-of-ways-to-place-houses.java
+++ 2397-count-number-of-ways-to-place-houses/count-number-of-ways-to-place-houses.java
@@ -1,20 +1,31 @@
 class Solution {
     int mod = 1000000007;
-
     public int countHousePlacements(int n) {
-       return (int) helper(n);
+        // if(n == 1){
+        //     return 4;
+        // }
+        long[] dp = new long[n+1];
+        Arrays.fill(dp,-1);
+        long ans = helper(n,dp);
+        long res = ans*ans;
+        return (int)(res%mod);
     }
+    public long helper(int n, long[] dp){
+        if( n < 0){
+            return 0;
+        }
+        if(n == 0){
+            return 1;
+        }
+        if(n == 1){
+            return 2;
+        }
 
-    public long helper(int n){
-         if(n == 1){
-            return 4;
+        if(dp[n] != -1){
+            return dp[n];
         }
-        int[] dp = new int[n];
-        dp[0] = 2;
-        dp[1] = 3;
-        for(int i = 2; i<n; i++){
-            dp[i] = (dp[i-1]+dp[i-2])%mod;
-        }
-        return   ((long)dp[n-1]*dp[n-1]) %mod;
+        long a = helper(n-1,dp);
+        long b = helper(n-2,dp);
+        return dp[n] = (a+b)%mod;  
     }
 }
