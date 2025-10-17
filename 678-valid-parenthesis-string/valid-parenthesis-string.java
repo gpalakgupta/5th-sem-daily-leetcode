e227b3867c6d46e7e1a3ad1c665a60a850aeb4a4
Palak gupta <151661890+gpalakgupta@users.noreply.github.com>
2025-10-17 11:59:48 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (78.52%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -1,51 +1,30 @@
 class Solution {
     public boolean checkValidString(String s) {
-        int min = 0;
-        int max = 0;
+        int low = 0;
+        int high = 0;
         for (char ch : s.toCharArray()) {
             if (ch == '(') {
-                min++;
-                max++;
+                low++;
+                high++;
             } else if (ch == ')') {
-                min--;
-                max--;
+                if (low > 0) {
+                    low--;
+                }
+                high--;
+
             } else {
-                min--;
-                max++;
+                if (low > 0) {
+                    low--;
+                }
+                high++;
             }
-            if (min < 0) {
-                min = 0;
-            }
-            if (max < 0) {
+            if (high < 0) {
                 return false;
             }
         }
-        return min == 0;
- 
-        // int n = s.length();
-        // Boolean[][] dp = new Boolean[n][n];
-        // return helper(0, 0, n, s, dp);
+
+        return low == 0;
     }
-
-    // public boolean helper(int idx, int cnt, int n, String str, Boolean[][] dp) {
-    //     if (cnt < 0) {
-    //         return false;
-    //     }
-    //     if (idx == n) {
-    //         return cnt == 0;
-    //     }
-    //     if (dp[idx][cnt] != null) {
-    //         return dp[idx][cnt];
-    //     }
-    //     char ch = str.charAt(idx);
-    //     if (ch == '(') {
-    //         return helper(idx + 1, cnt + 1, n, str, dp);
-    //     }
-    //     if (ch == ')') {
-    //         return helper(idx + 1, cnt - 1, n, str, dp);
-    //     }
-    //     return dp[idx][cnt] = helper(idx + 1, cnt + 1, n, str, dp) || helper(idx + 1, cnt - 1, n, str, dp)
-    //             || helper(idx + 1, cnt, n, str, dp);
-
-    // }
 }
+
+// 
