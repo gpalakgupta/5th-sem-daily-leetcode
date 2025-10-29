a103f496cab6b4e5f5c8f8a5fd906b5f4d81f7e4
Palak gupta <151661890+gpalakgupta@users.noreply.github.com>
2025-10-29 09:59:38 +0530

    Time: 6 ms (17.54%) | Memory: 44.8 MB (55.60%) - LeetSync

--- 815-champagne-tower/champagne-tower.java
+++ 815-champagne-tower/champagne-tower.java
@@ -4,28 +4,27 @@ class Solution {
         for(double[] a : dp){
             Arrays.fill(a,-1);
         }
-        return Math.min(1.0,helper(poured,query_row,query_glass,dp));
+        return Math.min(1.0,helper(poured, query_row, query_glass,dp));
     }
-    public double helper(int poured, int i, int j,double[][] dp){
-        if(i < 0 || j < 0 || i < j){
+    public double helper(int poured, int i, int j, double[][] dp){
+        if(i<0 || j < 0 || i<j){
             return 0.0;
         }
+
         if(i == 0 && j == 0){
             return poured;
         }
-
         if(dp[i][j] != -1){
             return dp[i][j];
         }
 
-        double left = (helper(poured,i-1,j-1,dp)-1)/2.0;
-        double right =( helper(poured,i-1,j,dp)-1)/2.0;
-
+        double left = (helper(poured, i-1,j-1,dp)-1)/2.0;
+        double right = (helper(poured, i-1,j,dp)-1)/2.0;
         if(left < 0){
-            left = 0.0;
+            left = 0;
         }
         if(right < 0){
-            right = 0.0;
+            right = 0;
         }
         return dp[i][j] = left+right;
     }
