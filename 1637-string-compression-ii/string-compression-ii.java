991862d2b9edeaa119b7dd34c70574b880f24471
Palak gupta <151661890+gpalakgupta@users.noreply.github.com>
2025-10-31 12:34:21 +0530

    Time: 1022 ms (5.08%) | Memory: 271.1 MB (5.07%) - LeetSync

--- 1637-string-compression-ii/string-compression-ii.java
+++ 1637-string-compression-ii/string-compression-ii.java
@@ -3,7 +3,7 @@ class Solution {
     int[][][][] dp;
     public int getLengthOfOptimalCompression(String s, int k) {
         n = s.length();
-        dp = new int[n][27][101][k+1];
+        dp = new int[n+1][27][101][k+1];
         for(int[][][] a : dp){
             for(int[][] b : a){
                 for(int[] c : b){
@@ -11,32 +11,33 @@ class Solution {
                 }
             }
         }
-        return helper(s,k,0,0,26);
+        return helper(s,k,0,26,0);
     }
-    public int helper(String s, int k, int idx, int freq, int pre_char){
+    public int helper(String s, int k, int idx, int pre_char,int freq){
+      
         if(k < 0){
             return Integer.MAX_VALUE;
         }
-        if(idx >= n){
+          if(idx >= n){
             return 0;
         }
-
+       
         if(dp[idx][pre_char][freq][k] != -1){
             return dp[idx][pre_char][freq][k];
         }
 
-        int delete = helper(s,k-1,idx+1,freq,pre_char);
+        int delete = helper(s,k-1,idx+1,pre_char,freq);
         int keep = 0;
-        if(s.charAt(idx) - 'a' == pre_char){
+        if(s.charAt(idx)-'a' == pre_char){
             int add = 0;
-            if(freq == 9 || freq == 99 || freq == 1){
+            if(freq == 1 || freq == 9 || freq == 99){
                 add = 1;
             }
-            keep = add + helper(s,k,idx+1,freq+1,pre_char);
+            keep = add+helper(s,k,idx+1,pre_char,freq+1);
         }
         else{
-            keep = 1+helper(s,k,idx+1,1,s.charAt(idx)-'a');
+            keep = 1+helper(s,k,idx+1,s.charAt(idx)-'a',1);
         }
-        return dp[idx][pre_char][freq][k] = Math.min(keep,delete);
+        return dp[idx][pre_char][freq][k] = Math.min(delete,keep);
     }
 }
