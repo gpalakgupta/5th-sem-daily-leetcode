bb3e4be0d741cd64b16a94cab14d34f437dad5d2
Palak gupta <151661890+gpalakgupta@users.noreply.github.com>
2025-11-01 08:49:36 +0530

    Time: 30 ms (47.65%) | Memory: 93.1 MB (5.04%) - LeetSync

--- 1554-minimum-time-to-collect-all-apples-in-a-tree/minimum-time-to-collect-all-apples-in-a-tree.java
+++ 1554-minimum-time-to-collect-all-apples-in-a-tree/minimum-time-to-collect-all-apples-in-a-tree.java
@@ -1,30 +1,24 @@
 class Solution {
     public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            map.put(i, new ArrayList<>());
+        HashMap<Integer,List<Integer>> map = new HashMap<>();
+        for(int i = 0; i<n; i++){
+            map.put(i,new ArrayList<>());
         }
-        for (int[] arr : edges) {
+        for(int[] arr : edges){
             int a = arr[0];
             int b = arr[1];
             map.get(a).add(b);
             map.get(b).add(a);
         }
-        HashSet<Integer> st = new HashSet<>();
-        st.add(0);
-        return dfs(map, hasApple, 0, st);
-
+        return dfs(map,hasApple,0,-1);
     }
-
-    public int dfs(HashMap<Integer, List<Integer>> map, List<Boolean> hasApple, int src, HashSet<Integer> st) {
-        
+    public int dfs(HashMap<Integer,List<Integer>> map, List<Boolean> hasApple, int src,int parent){
         int total = 0;
-        for (int nbrs : map.get(src)) {
-            if (st.contains(nbrs)) {
+        for(int nbrs : map.get(src)){
+            if(nbrs == parent){
                 continue;
             }
-            st.add(nbrs);
-            int time = dfs(map, hasApple, nbrs, st);
+            int time = dfs(map,hasApple,nbrs,src);
             if(time > 0 || hasApple.get(nbrs)){
                 total += time+2;
             }
