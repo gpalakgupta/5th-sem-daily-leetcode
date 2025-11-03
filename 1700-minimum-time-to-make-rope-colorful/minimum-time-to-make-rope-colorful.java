1347ea21441ee0d51680ea8337459674ef25e9cc
Palak gupta <151661890+gpalakgupta@users.noreply.github.com>
2025-11-03 09:29:35 +0530

    Time: 80 ms (5.24%) | Memory: 211.4 MB (5.68%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
@@ -1,17 +1,20 @@
+import java.util.Arrays;
+
 class Solution {
     int n;
-    
+    int[][] dp;
+
     public int minCost(String colors, int[] neededTime) {
         n = colors.length();
-        
+        dp = new int[n][27];
+        for (int[] a : dp) Arrays.fill(a, -1);
         return helper(colors, neededTime, 0, 26, 0);
     }
 
-    
     public int helper(String colors, int[] need, int idx, int prevColor, int prevTime) {
         if (idx >= n) return 0;
+        if (dp[idx][prevColor] != -1) return dp[idx][prevColor];
 
-         
         int res;
         int currColor = colors.charAt(idx) - 'a';
         int currTime = need[idx];
@@ -25,6 +28,6 @@ class Solution {
             res = helper(colors, need, idx + 1, currColor, currTime);
         }
 
-        return res;
+        return dp[idx][prevColor] = res;
     }
 }
