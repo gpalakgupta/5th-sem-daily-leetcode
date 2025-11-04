6f86baaf863eb6c454fb6c22ccc1be2987b4d6a6
Palak gupta <151661890+gpalakgupta@users.noreply.github.com>
2025-11-04 11:43:41 +0530

    Time: 287 ms (5.09%) | Memory: 240.4 MB (5.09%) - LeetSync

--- 2364-longest-path-with-different-adjacent-characters/longest-path-with-different-adjacent-characters.java
+++ 2364-longest-path-with-different-adjacent-characters/longest-path-with-different-adjacent-characters.java
@@ -1,45 +1,43 @@
 class Solution {
     int res;
-
-    public int dfs(HashMap<Integer, List<Integer>> map, int curr, int parent, String s) {
-        int longest = 0;
-        int sec_long = 0;
-        for (int nbrs : map.get(curr)) {
-            if (nbrs == parent) {
-                continue;
-            }
-            int childLen = dfs(map, nbrs, curr, s);
-            if (s.charAt(nbrs) == s.charAt(curr)) {
-                continue;
-            }
-          
-            if (childLen > longest) {
-                sec_long = longest;
-                longest = childLen;
-            } else if (childLen > sec_long) {
-                sec_long = childLen;
-            }
-
-        }
-      
-        res = Math.max(res, longest + sec_long + 1);
-        return longest + 1;
-    }
-
+   
     public int longestPath(int[] parent, String s) {
-        int n = parent.length;
         res = 0;
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            map.put(i, new ArrayList<>());
+        int n = parent.length;
+        HashMap<Integer,List<Integer>> map = new HashMap<>();
+        for(int i = 0; i<n; i++){
+            map.put(i,new ArrayList<>());
         }
-        for (int i = 1; i < n; i++) {
+        for(int i = 1; i<n; i++){
             int a = i;
             int b = parent[i];
             map.get(a).add(b);
             map.get(b).add(a);
         }
-        dfs(map, 0, -1, s);
+
+        dfs(map,0,-1,s);
         return res;
+    }
+    public int dfs(HashMap<Integer,List<Integer>> map, int curr, int parent, String str){
+        int longest = 0;
+        int sec_long = 0;
+        for(int nbrs : map.get(curr)){
+            if(nbrs == parent){
+                continue;
+            }
+            int child = dfs(map,nbrs,curr,str);
+            if(str.charAt(nbrs) == str.charAt(curr)){
+                continue;
+            }
+            if(child > longest){
+                sec_long = longest;
+                longest = child;
+            }
+            else if(child > sec_long){
+                sec_long = child;
+            }
+        }
+        res = Math.max(res,longest+sec_long+1);
+        return longest+1;
     }
 }
