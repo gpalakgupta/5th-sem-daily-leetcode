92b07ffd961e32bb481df7b0f105da751687cf2a
Palak gupta <151661890+gpalakgupta@users.noreply.github.com>
2025-11-04 11:03:05 +0530

    Time: 57 ms (5.38%) | Memory: 47.1 MB (5.01%) - LeetSync

--- 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.java
+++ 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.java
@@ -1,40 +1,38 @@
 class Solution {
-    
-
     public String smallestEquivalentString(String s1, String s2, String baseStr) {
         int n = s1.length();
-        HashMap<Character, List<Character>> map = new HashMap<>();
-        for (char ch = 'a'; ch <= 'z'; ch++) {
-            map.put(ch, new ArrayList<>());
+        int m = baseStr.length();
+        HashMap<Character,List<Character>> map = new HashMap<>();
+        for(char ch = 'a'; ch <='z'; ch++){
+            map.put(ch,new ArrayList<>());
         }
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i<n; i++){
             char a = s1.charAt(i);
             char b = s2.charAt(i);
             map.get(a).add(b);
             map.get(b).add(a);
         }
+
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < baseStr.length(); i++) {
+        for(int i = 0; i<m; i++){
             HashSet<Character> st = new HashSet<>();
-            char ch = dfs(baseStr.charAt(i), map, st);
-            sb.append(ch);
+            char min = dfs(map,st,baseStr.charAt(i));
+            sb.append(min);
         }
         return sb.toString();
     }
-
-    public char dfs(char ch, HashMap<Character, List<Character>> map, HashSet<Character> visited) {
-        visited.add(ch);
-        char ans = ch;
-        for (char nbrs : map.get(ch)) {
-            if(visited.contains(nbrs)){
+    public char dfs(HashMap<Character,List<Character>> map, HashSet<Character> st, char ch){
+        char min = ch;
+        st.add(min);
+        for(char nbrs : map.get(ch)){
+            if(st.contains(nbrs)){
                 continue;
             }
-            
-            char min  =  dfs(nbrs,map,visited);
-            if(min < ans){
-                ans = min;
+            char t = dfs(map,st,nbrs);
+            if(t < min){
+                min = t;
             }
         }
-        return ans;
+        return min;
     }
 }
