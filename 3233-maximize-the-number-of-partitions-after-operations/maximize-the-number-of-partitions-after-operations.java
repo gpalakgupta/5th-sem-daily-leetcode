9673300fd2dcd739a73527c88c65ba0fbef5e059
Palak gupta <151661890+gpalakgupta@users.noreply.github.com>
2025-11-04 14:26:22 +0530

    Time: 623 ms (5.00%) | Memory: 195.5 MB (5.01%) - LeetSync

--- 3233-maximize-the-number-of-partitions-after-operations/maximize-the-number-of-partitions-after-operations.java
+++ 3233-maximize-the-number-of-partitions-after-operations/maximize-the-number-of-partitions-after-operations.java
@@ -1,40 +1,44 @@
 class Solution {
-    HashMap<String, Integer> map = new HashMap<>();
-
+    HashMap<String,Integer> map = new HashMap<>();
     public int maxPartitionsAfterOperations(String s, int k) {
-        return helper(s,k,0,0,true)+1;
+        map.clear();
+        return helper(s,k,0,-1,true);
     }
-
-    public int helper(String s, int k, int idx, long uni_char, boolean canPart) {
-        if (idx >= s.length()) {
+    public int helper(String s, int k, int idx, long uniChar, boolean canPart){
+        if(idx >= s.length()){
             return 0;
         }
-        String key = idx+"-"+uni_char+"-"+canPart;
-        if (map.containsKey(key)) {
+
+
+        String key = idx+"-"+uniChar+"-"+canPart;
+        if(map.containsKey(key)){
             return map.get(key);
         }
-        int charIdx = s.charAt(idx) - 'a';
-        long charUpdate = uni_char | (1L << charIdx);
-        long uniCnt = Long.bitCount(charUpdate);
 
-        int result = 0;
-        if (uniCnt > k) {
-            result = 1 + helper(s, k, idx + 1, 1L << charIdx, canPart);
-        } else {
-            result = helper(s, k, idx + 1, charUpdate, canPart);
+        int charIdx = s.charAt(idx)-'a';
+        long canUpdate = uniChar | (1L << charIdx);
+        long unicnt = Long.bitCount(canUpdate);
+        int res = 0;
+        if(unicnt > k){
+            res = 1+ helper(s,k,idx+1, 1 << charIdx ,canPart);
         }
-        if (canPart) {
-            for (int i = 0; i < 26; i++) {
-                long newcharUpdate = uni_char | (1L << i);
-                long newuniCnt = Long.bitCount(newcharUpdate);
-                if (newuniCnt > k) {
-                    result = Math.max(result,1 + helper(s, k, idx + 1, 1L << i, false));
-                } else {
-                    result = Math.max(result,helper(s, k, idx + 1, newcharUpdate, false));
+        else{
+            res = helper(s,k,idx+1,canUpdate,canPart);
+        }
+
+        if(canPart){
+            for(int i = 0; i<26; i++){
+                long newcharidx = uniChar | (1L << i);
+                long newcnt = Long.bitCount(newcharidx);
+                if(newcnt > k){
+                    res = Math.max(res,1+helper(s,k,idx+1, 1L<<i, false));
+                }
+                else{
+                    res = Math.max(res,helper(s,k,idx+1,newcharidx,false));
                 }
             }
         }
-        map.put(key,result);
-        return result;
+        map.put(key,res);
+        return res;
     }
 }
